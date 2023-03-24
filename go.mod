320abc09c708a06f5e52e2d322585f5794f462aa
tiancheng.xu <kiritoeva@icloud.com>
2023-03-24 09:59:46 +0800

    feat: 添加sonic支持

--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module github.com/Yostardev/json
 go 1.19
 
 require (
+	github.com/bytedance/sonic v1.8.6
 	github.com/goccy/go-json v0.10.2
 	github.com/json-iterator/go v1.1.12
 )
 
 require (
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 )
--- jsoniter.go
+++ jsoniter.go
@@ -6,10 +6,15 @@ package json
 import jsoniter "github.com/json-iterator/go"
 
 var (
-	json          = jsoniter.ConfigCompatibleWithStandardLibrary
-	Marshal       = json.Marshal
-	Unmarshal     = json.Unmarshal
+	json = jsoniter.ConfigCompatibleWithStandardLibrary
+	// Marshal is exported by gin/json package.
+	Marshal = json.Marshal
+	// Unmarshal is exported by gin/json package.
+	Unmarshal = json.Unmarshal
+	// MarshalIndent is exported by gin/json package.
 	MarshalIndent = json.MarshalIndent
-	NewDecoder    = json.NewDecoder
-	NewEncoder    = json.NewEncoder
+	// NewDecoder is exported by gin/json package.
+	NewDecoder = json.NewDecoder
+	// NewEncoder is exported by gin/json package.
+	NewEncoder = json.NewEncoder
 )
